26f0cffae384c0499040c3bd6d13cbcd4e8d499f
Samuel Hindmarsh <samuel.hindmarsh@trademe.co.nz>
2014-11-07 12:08:43 +1300

    Basic RecyclerView

--- magiccardwall/app/src/main/java/nz/co/trademe/fedex5/magiccardwall/fragment/HistoryFragment.java
+++ magiccardwall/app/src/main/java/nz/co/trademe/fedex5/magiccardwall/fragment/HistoryFragment.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -21,11 +23,17 @@ import nz.co.trademe.fedex5.magiccardwall.R;
 
 public class HistoryFragment extends Fragment {
 
+	private static final String TAG = HistoryFragment.class.getSimpleName();
+
 	private RecyclerView recyclerView;
 	private RecyclerView.LayoutManager layoutManager;
 	private RecyclerView.Adapter adapter;
 
 	public HistoryFragment() {}
+
+	private enum Filter {
+		EVERYONE, ME
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -45,13 +53,22 @@ public class HistoryFragment extends Fragment {
 				android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		filter.setAdapter(adapter);
+		filter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				setFilter(Filter.values()[position]);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {}
+		});
 
 		recyclerView = (RecyclerView) v.findViewById(R.id.recyclerHistory);
 
 		layoutManager = new LinearLayoutManager(getActivity());
 		recyclerView.setLayoutManager(layoutManager);
 
-		this.adapter = new HistoryAdapter();
+		setFilter(Filter.EVERYONE);
 
 		return v;
 	}
@@ -66,30 +83,56 @@ public class HistoryFragment extends Fragment {
 		integrator.initiateScan();
 	}
 
+	private void setFilter(Filter filter) {
+		switch (filter) {
+			case ME:
+				Log.d(TAG, "filter by ME");
+				// todo me
+				break;
+			case EVERYONE:
+			default:
+				Log.d(TAG, "filter by EVERYONE");
+				// todo everyone
+
+				adapter = new HistoryAdapter(new String[]{"Hello", "Bonjour", "Hallo"});
+				break;
+		}
+
+		recyclerView.setAdapter(adapter);
+	}
+
+
 	private class HistoryAdapter extends RecyclerView.Adapter {
 
-		private class ViewHolder extends RecyclerView.ViewHolder {
-			public TextView mTextView;
+		class ViewHolder extends RecyclerView.ViewHolder {
+			TextView tv;
 
-			public ViewHolder(TextView v) {
+			public ViewHolder(View v) {
 				super(v);
-				mTextView = v;
+				tv = (TextView) v.findViewById(R.id.cardTextViewTest);
 			}
+		}
+
+		private String[] data;
+
+		public HistoryAdapter(String[] data) {
+			this.data = data;
 		}
 
 		@Override
 		public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-			return null;
+			return new ViewHolder(LayoutInflater.from(getActivity()).inflate(R.layout.card_history, viewGroup, false));
 		}
 
 		@Override
 		public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
-
+			ViewHolder holder = (ViewHolder) viewHolder;
+			holder.tv.setText(data[i]);
 		}
 
 		@Override
 		public int getItemCount() {
-			return 0;
+			return data.length;
 		}
 	}
 
